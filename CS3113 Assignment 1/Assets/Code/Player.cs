50813700a5a26fbfa0461750a8ad689405236d7f
elijah-whittle <edw2290@nyu.edu>
2021-10-05 22:54:20 -0400

    Add Starting Project
    added a project with the basic controls and starting sample scenes for each element.

---
+++ CS3113 Assignment 1/Assets/Code/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int speed = 4;
+    public int jumpForce = 500;
+
+    public LayerMask groundLayer;
+    public Transform feet;
+    public bool grounded = false;
+
+    float xSpeed;
+    Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        xSpeed = Input.GetAxis("Horizontal") * speed;
+        _rigidbody.velocity = new Vector2(xSpeed, _rigidbody.velocity.y);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            transform.position = Vector2.zero;
+        }
+
+        if ((xSpeed < 0 && transform.localScale.x > 0) || (xSpeed > 0 && transform.localScale.x < 0))
+        {
+            transform.localScale *= new Vector2(-1, 1);
+        }
+
+        grounded = Physics2D.OverlapCircle(feet.position, .3f, groundLayer);
+
+        if(grounded && Input.GetButtonDown("Jump"))
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+            _rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+
+    }
+}
