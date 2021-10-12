87051de9c1c9a49f1c61f629f83a513efba476c9
FantasiA10 <yw4177@nyu.edu>
2021-10-12 16:23:04 -0400

    add wind spell in player

--- CS3113 Assignment 1/Assets/Code/Player.cs
+++ CS3113 Assignment 1/Assets/Code/Player.cs
@@ -58,12 +58,24 @@ public class Player : MonoBehaviour
 
     /*-------------------------------*/
 
+    /*------------ WIND ------------*/
+    public GameObject Wind_blade;
+    public Transform wind_blade_pos;
+    public GameObject Wind_shield;
+    bool Wind_power = true;
+    void closewindshield(){
+        Wind_shield.GetComponent<Renderer>().enabled = false;
+                Wind_shield.GetComponent<Collider2D>().enabled = false;
+    }
+    /*-------------------------------*/
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         //lightPos = GetComponent<Transform>();
+        Wind_shield.GetComponent<Renderer>().enabled = false;
+        Wind_shield.GetComponent<Collider2D>().enabled = false;
     }
 
     void FixedUpdate()
@@ -122,6 +134,20 @@ public class Player : MonoBehaviour
             Destroy(light, 10.0f);
         }
 
+        /* Wind */
+        if(Input.GetKeyDown (KeyCode.I)){
+            if(Wind_power){
+                GameObject newwind_blade = Instantiate(Wind_blade,wind_blade_pos.position,Quaternion.identity);
+            }
+        }
+        if(Input.GetKeyDown (KeyCode.O)){
+            if(Wind_power){
+                Wind_shield.GetComponent<Renderer>().enabled = true;
+                Wind_shield.GetComponent<Collider2D>().enabled = true;
+                Invoke("closewindshield", 2);
+            }
+        }
+
         // jump
         if ((grounded || earthTouch) && Input.GetButtonDown("Jump"))
         {
@@ -150,6 +176,9 @@ public class Player : MonoBehaviour
                 paperCollected = false;
                 SceneManager.LoadScene(levels[currentLevel]);
             }
+        }
+        if(collision.tag == "wind_pill"){
+            Wind_power = true;
         }
 
     }
