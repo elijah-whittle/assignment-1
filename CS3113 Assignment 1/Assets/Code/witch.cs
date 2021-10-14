29a4138324c448de91063bc6ff9b7d100b2f447d
FantasiA10 <yw4177@nyu.edu>
2021-10-13 23:06:39 -0400

    witch motion changes

--- CS3113 Assignment 1/Assets/Code/witch.cs
+++ CS3113 Assignment 1/Assets/Code/witch.cs
@@ -2,53 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class witch : MonoBehaviour
-{
+public class witch : MonoBehaviour{
     public int dir = 1;
     public float health = 50f;
     public GameObject Player;
     public Transform fire_pos;
     public GameObject Bullet1;
-    public Animator witchanimator;
-    public float speed = 1f;
-    // Start is called before the first frame update
-    private bool isattack = false;
-    private bool left = true;
+    Animator _animator;
+    Rigidbody2D _rigidbody;
+    public float Speed = 0.5f;
+    private int direction = -1;
+    enum State{
+        stay,
+        move,
+        attack,
+        stage2
+    }
+    State currentState;
     void lookplayer(){
         if (gameObject.transform.position.x < Player.transform.position.x){
             transform.eulerAngles = new Vector3(0,180,0);
-            left = false;
+            direction = 1;
         }
         else{
             transform.eulerAngles = new Vector3(0,0,0);
-            left = true;
+            direction = -1;
         }
     }
-    void stay(){
-        lookplayer();
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f,0f);
-        isattack = false;
-    }
-    void move(){
-        lookplayer();
-        if (left == true){
-            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speed*-1,0f);
-        }
-        else{gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speed,0f);}
-        isattack = false;
-    }
-    void attack(){
-        lookplayer();
-        isattack = true;
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f,0f);
-    }
-    void Start()
-    {
+    private void Start(){
         //Vector2 currPos = transform.position;
         //float moveRight = transform.position.x;
         //float moveLeft = -transform.position.x;
-        //Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        StartCoroutine(NewState());
     }
+    IEnumerator NewState(){
+        while(health>=25){
+            _animator.SetFloat("health", health);
+            yield return new WaitForSeconds(3);
+            currentState = (State)Random.Range(0,3);
+        }
+    }
+    void Attack1(){
+        _animator.SetTrigger("Attack1");
+    }
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Spell"))
@@ -58,40 +57,37 @@ public class witch : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        witchanimator.SetFloat("speed", Mathf.Abs(gameObject.GetComponent<Rigidbody2D>().velocity.x));
-        witchanimator.SetBool("is_attack1", isattack);
-        witchanimator.SetFloat("health", health);
-        if(health >= 25){
-            int rand = Random.Range(0,5);
-            if (rand==0){
-                stay();
-            }
-            else if (rand==1){
-                move();
+    private void FixedUpdate(){
+        lookplayer();
+        switch(currentState){
+            case State.move:
+                _animator.SetBool("is_attack1", false);
+                _rigidbody.velocity = new Vector2(Speed*direction, 0);
+                _animator.SetFloat("speed", Mathf.Abs(_rigidbody.velocity.x));
+                break;
+            case State.stay:
+                _animator.SetBool("is_attack1", false);
+                _rigidbody.velocity = new Vector2(0.0f,0.0f);
+                _animator.SetFloat("speed", Mathf.Abs(gameObject.GetComponent<Rigidbody2D>().velocity.x));
+                break;
+            case State.attack:
+                Attack1();
+                _rigidbody.velocity = new Vector2(0.0f,0.0f);
+                StartCoroutine(attackmotion());
+                break;
+            default:
+                break;
+        }
+    }
+    IEnumerator attackmotion(){
+        while (true){
+            if(_animator.GetCurrentAnimatorStateInfo(0).normalizedTime>1.0f){
+                    break;
             }
             else{
-                attack();
+                _rigidbody.velocity = new Vector2(0.0f,0.0f);
             }
+            yield return new WaitForSeconds(0);
         }
-        if(health <= 25 & health>0){
-            int rand = Random.Range(0,5);
-            if (rand==0){
-                stay();
-            }
-            else if (rand==1){
-                move();
-            }
-            else{
-                attack();
-            }
-        }
-
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
-        //gameObject.position.x += enemySpeed;
     }
 }
