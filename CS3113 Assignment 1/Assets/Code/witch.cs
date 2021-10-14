e7776b322d6db0b15725757850f3ab45b1002644
FantasiA10 <yw4177@nyu.edu>
2021-10-14 01:25:52 -0400

    witch code modify

--- CS3113 Assignment 1/Assets/Code/witch.cs
+++ CS3113 Assignment 1/Assets/Code/witch.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class witch : MonoBehaviour{
     public int dir = 1;
-    public float health = 50f;
+    public float health = 50;
     public GameObject Player;
     public Transform fire_pos;
     public GameObject Bullet1;
+    public Transform bulletpos;
+    public int castingForce = 50;
+    public float Timebetween = 0.5f;
     Animator _animator;
     Rigidbody2D _rigidbody;
-    public float Speed = 0.5f;
-    private int direction = -1;
+    public float Speed = 0.7f;
+
+    private int direction = -1; 
+    float _Timer;
+
     enum State{
         stay,
         move,
         attack,
-        stage2
+        stage2,
+        stay2,
+        move2,
+        attack2,
+        attack3
     }
     State currentState;
     void lookplayer(){
@@ -38,42 +48,68 @@ public class witch : MonoBehaviour{
         StartCoroutine(NewState());
     }
     IEnumerator NewState(){
-        while(health>=25){
-            _animator.SetFloat("health", health);
-            yield return new WaitForSeconds(3);
+        while(health>25){
+            yield return new WaitForSeconds(1.5f);
             currentState = (State)Random.Range(0,3);
+        }
+        while (health<25 & health>0){
+            yield return new WaitForSeconds(3);
+            currentState = (State)Random.Range(4,8);
         }
     }
     void Attack1(){
         _animator.SetTrigger("Attack1");
+    }
+    void Attack2(){
+        _animator.SetTrigger("Attack2");
     }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Spell"))
         {
-            health -= 1;
+            //health -= 1;
         }
     }
 
     // Update is called once per frame
     private void FixedUpdate(){
+        _animator.SetFloat("health", health);
         lookplayer();
         switch(currentState){
             case State.move:
-                _animator.SetBool("is_attack1", false);
                 _rigidbody.velocity = new Vector2(Speed*direction, 0);
                 _animator.SetFloat("speed", Mathf.Abs(_rigidbody.velocity.x));
                 break;
             case State.stay:
-                _animator.SetBool("is_attack1", false);
                 _rigidbody.velocity = new Vector2(0.0f,0.0f);
                 _animator.SetFloat("speed", Mathf.Abs(gameObject.GetComponent<Rigidbody2D>().velocity.x));
                 break;
             case State.attack:
-                Attack1();
                 _rigidbody.velocity = new Vector2(0.0f,0.0f);
-                StartCoroutine(attackmotion());
+                _Timer+=Time.deltaTime;
+                if (_Timer>Timebetween){
+                    GameObject newBlast = Instantiate(Bullet1, bulletpos.position, Quaternion.identity);
+                    newBlast.GetComponent<Rigidbody2D>().AddForce(new Vector2(castingForce * transform.localScale.x*direction, 0));
+                    _Timer = 0;
+                }
+                Attack1();
+                break;
+            case State.move2:
+                _rigidbody.velocity = new Vector2(Speed*direction, 0);
+                _animator.SetFloat("speed", Mathf.Abs(_rigidbody.velocity.x));
+                break;
+            case State.stay2:
+                _rigidbody.velocity = new Vector2(0.0f,0.0f);
+                _animator.SetFloat("speed", Mathf.Abs(gameObject.GetComponent<Rigidbody2D>().velocity.x));
+                break;
+            case State.attack2:
+                _rigidbody.velocity = new Vector2(0.0f,0.0f);
+                Attack2();
+                break;
+            case State.attack3:
+                _rigidbody.velocity = new Vector2(0.0f,0.0f);
+                Attack2();
                 break;
             default:
                 break;
