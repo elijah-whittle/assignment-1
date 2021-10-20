945c7b08438ce249e7bc6a265b02dac0bedf14d3
FantasiA10 <yw4177@nyu.edu>
2021-10-20 02:41:26 -0400

    intro update

--- CS3113 Assignment 1/Assets/Code/intro_UI_controller.cs
+++ CS3113 Assignment 1/Assets/Code/intro_UI_controller.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class intro_UI_controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject move_instruction;
+    public GameObject earth_instruction;
+    public GameObject light_instruction;
+    public GameObject attack_instruction;
+
+    public Transform master_earth;
+    public Transform master_light;
+    public Transform master_attack;
+    private float Player_trans;
+    private int stage = 0;
     void Start()
     {
         
+        Player_trans = gameObject.GetComponent<Transform>().position.x;
+        move_instruction.SetActive(true);
+        earth_instruction.SetActive(false);
+        light_instruction.SetActive(false);
+        attack_instruction.SetActive(false);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Player_trans = gameObject.GetComponent<Transform>().position.x;
+        if(stage == 0){
+            if(Player_trans>=master_earth.position.x){
+                move_instruction.SetActive(false);
+                earth_instruction.SetActive(true);
+                stage+=1;
+            }
+        }
+        if (stage == 1){
+            if(Player_trans>=master_light.position.x){
+                earth_instruction.SetActive(false);
+                light_instruction.SetActive(true);
+                stage+=1;
+            }
+        }
+        if (stage == 2){
+            if(Player_trans>=master_attack.position.x){
+                light_instruction.SetActive(false);
+                attack_instruction.SetActive(true);
+                stage+=1;
+            }
+        }
     }
 }
