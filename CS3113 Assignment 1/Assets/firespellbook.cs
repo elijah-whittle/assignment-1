ae057f6173c09b3af60629392909290897d79f0c
jp5169 <jp5169@nyu.edu>
2021-10-20 21:06:57 -0400

    fire spell book look and animation

---
+++ CS3113 Assignment 1/Assets/firespellbook.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firespellbook : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Animator sbAni;
+
+    void Start()
+    {
+        sbAni = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
